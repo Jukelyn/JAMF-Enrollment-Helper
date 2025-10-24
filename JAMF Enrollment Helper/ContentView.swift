b7125e17c2cfa6e916b4fcc0da8e787c28bfefed
Jukelyn <viperkillergaming@gmail.com>
2025-10-24 11:21:49 -0400

    Initial Commit

---
+++ JAMF Enrollment Helper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JAMF Enrollment Helper
+//
+//  Created by Mehraz Ahmed on 10/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JAMF Enrollment Helper/JAMF_Enrollment_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  JAMF_Enrollment_HelperApp.swift
+//  JAMF Enrollment Helper
+//
+//  Created by Mehraz Ahmed on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct JAMF_Enrollment_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
